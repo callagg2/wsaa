00eb8106c99e69ee784a250c7e8d366bbc94daf7
callagg2 <gerrycallaghan2@gmail.com>
2026-02-21 20:51:54 +0000

    assignment 4 completed

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -3,25 +3,49 @@
 
 # Author: gerry callaghan
 
+from github import Github # to use this install package pip install PyGithub
+
+# Authentication is defined via github.Auth
+from github import Auth
+
 import requests
-import json
 from config import config as cfg
 
-filename = "instances_of_andrew"
+# importing my github key from a config file, which is not included in the repository, and which is ignored by git
+apikey = cfg["githubkey"]
 
-url = "https://api.github.com/repos/callagg2/aprivateone/contents/gemini_created_json_with_instances_of_andrew.json"
-#url = "https://api.github.com/repos/callagg2/wsaa"
-api_key = cfg["github_key"]
-response = requests.get(url, auth = ('token', api_key))
+# basing the following code on the example
+# https://pygithub.readthedocs.io/en/latest/examples/Repository.html#update-a-file-in-the-repository
 
-print(response.status_code)
-#print(response.json())
+# using a fine-grained token I set up on Github
 
-#gemini_created_json_with_instances_of_andrew.json
+#authorization token based on my apikey
+g = Github(auth=Auth.Token(apikey))
 
-#'repo = 
-#fileInfo = repo.get_contents("test.txt")
+# use the token to access the repository
+repo = g.get_repo("callagg2/aprivateone")
 
-with open(filename, "w") as fp:
-        data = response.json()
-        json.dump(data, fp, indent=4)
+# set the variable contents to the file I want to edit
+fileInfo = repo.get_contents("gemini_created_json_with_instances_of_andrew.json")
+
+# now I have the file, I can get the url to download it, and then get the contents of the file
+urlOfFile = fileInfo.download_url
+
+# set response to the contents of the file
+response = requests.get(urlOfFile)
+# set contentOfFile to the text of the file
+contentOfFile = response.text
+#print (contentOfFile) # to preview the contents of the file
+
+# make the change to the file, replacing all instances of "Andrew" with "Gerry"
+newContents = contentOfFile.replace("Andrew", "Gerry")
+# print (newContents) # to preview the changes
+
+# reupload the file with the changes, and commit those changes to the repository
+#  where fileInfo.path is the path to the file,
+# "updated by prog" is the commit message,  
+# newContents is the new content of the file, and
+# fileInfo.sha is the sha of the file, which is needed to make sure we are updating the correct version of the file
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",newContents,fileInfo.sha)
+# print (gitHubResponse) # just to see the file has been updated.
+
